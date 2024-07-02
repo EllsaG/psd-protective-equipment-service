a456aad50577e150ddc39cad29bd49ae494d2c9d
EllsaG <114601257+EllsaG@users.noreply.github.com>
2024-07-03 00:31:18 +0400

    Add tests files to the project. (#7)
    Co-authored-by: EllsaG <seninlsa@gmail.com>

---
+++ src/test/resources/sql/addProtectiveEquipmentSelectionInformation.sql
@@ -0,0 +1,6 @@
+insert into protective_equipment_selection (protective_equipment_selection_id,
+                                            calculated_current_of_electromagnetic_release,
+                                            calculated_current_of_thermal_release, equipment_rated_current,
+                                            equipment_starting_current)
+values (1, 305.6, 61.12, 70.29, 382),
+       (2, 154.7, 40.2, 30.2, 230);
---
+++ src/test/resources/sql/clearProtectiveEquipmentDB.sql
@@ -0,0 +1,4 @@
+delete from protective_equipment
+where protective_equipment_id>0;
+delete from low_volt_cables
+where low_volt_cables_id>0;
